237bdb8dce769f1ea18a38c37032e2ee877ba423
Jose Milla <josmilla@gmail.com>
2025-08-11 17:49:50 -0500

    Mi primer archivo java

---
+++ TallerJava/src/Semanas/Practica_1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semanas;
+
+/**
+ *
+ * @author jlmil
+ */
+public class Practica_1 {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo UTP");
+    }
+}
