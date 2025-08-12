da0523fcee43c8cf89c72fcc2ee218e99d5bddf3
Jose Milla <josmilla@gmail.com>
2025-08-11 19:56:31 -0500

    variables java

---
+++ TallerProgramacionJava/src/tallerutpjava/variables.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tallerutpjava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jlmil
+ */
+public class variables {
+    public static void main(String[] args) {
+       System.out.println("Hola Mundo");
+       Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa tu nombre: ");
+         String nombre = scanner.nextLine();
+     System.out.println("Tu nombre es: " + nombre);
+
+    }
+}
